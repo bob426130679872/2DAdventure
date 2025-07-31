42bb58dcfb78fe19068fb1083152501e7e857908
bob426130679872 <78817091+bob426130679872@users.noreply.github.com>
2025-07-31 14:36:39 +0800

    增加註解

--- GameManager.cs
+++ GameManager.cs
@@ -8,15 +8,15 @@ public class GameManager : MonoBehaviour
 
     public GameObject player;
     public GameObject playerPrefab;
-    public float health;
-    public string saveScene;
+    public float health;//玩家血量
+    public string saveScene;//玩家存檔點名稱(場景名)
     
-    public string spawnPortalName;
+    public string spawnPortalName;//玩家接下來要生成的生成點名稱
     public string playerName;
     public bool playerFlip = false;
 
-    public Vector3 safePosition;
-    public string safeSceneName;
+    public Vector3 safePosition;//玩家最近站立的位置
+    public string safeSceneName;//玩家最近站立的場景(目前沒用到)
 
     void Awake()
     {
